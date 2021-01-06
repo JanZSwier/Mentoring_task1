be2ec0981f56db318f3501e6d164f2d820b92e55
JanZSwier <jan.zbigniew.swierczynski@gmail.com>
2021-01-06 13:54:54 +0100

    made function for parser and for computaton

--- covid.py
+++ covid.py
@@ -3,14 +3,27 @@ import json
 import requests
 
 
-def main():
+def parser():
     my_parser = argparse.ArgumentParser(description='By giving name of country you will get '
                                                     'percentage of people recovered and still sick from all population')
-
     my_parser.add_argument('--country', action='store', required=True, help="Name of country"
                                                                             " begins with BIG LETTER")
+    return my_parser.parse_args()
 
-    args = my_parser.parse_args()
+
+def computing_answer(population, confirmed_cases, recovered_people, dead_people):
+    percent_of_recovered = round(100 * recovered_people / population, 1)
+    percent_of_sick = round(100 * (confirmed_cases - recovered_people - dead_people) / population, 1)
+    print(json.dumps(
+        {
+            'recovered': percent_of_recovered,
+            'sick': percent_of_sick
+        }
+        , indent=1))
+
+
+def main():
+    args = parser()
 
     response = requests.get(url="https://covid-api.mmediagroup.fr/v1/cases?country=" + args.country)
 
@@ -21,22 +34,10 @@ def main():
         all_data = countries_data.get("All")
 
         try:
-            population = all_data.get("population")
-            dead_people = all_data.get("deaths")
-            recovered_people = all_data.get("recovered")
-            confirmed_cases = all_data.get("confirmed")
 
-            percent_of_recovered = round(100 * recovered_people / population, 1)
-            percent_of_sick = round(100 * (confirmed_cases - recovered_people - dead_people) / population, 1)
+            computing_answer(all_data.get("population"), all_data.get("confirmed"),
+                             all_data.get("recovered"), all_data.get("deaths"))
 
-            relevant_info = {
-                'recovered': percent_of_recovered,
-                'sick': percent_of_sick
-            }
-
-            json_object = json.dumps(relevant_info, indent=1)
-
-            print(json_object)
         except AttributeError:
             print("Probably there is no such country as:\n" + args.country + "\n;)")
     else:
