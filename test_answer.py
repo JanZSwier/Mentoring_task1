f99ece27c47576f22bb07bafc299677a0f87d476
Paweł Klos <p.klos@piwik.pro>
2021-01-20 18:09:14 +0100

    ohno

--- test_answer.py
+++ test_answer.py
@@ -4,6 +4,8 @@ from covid import compute_answer
 
 
 def test_compute_answer():
-    assert json.dumps({"recovered": 5.0, "sick": 4.0}, indent=1) == compute_answer(
+    assert json.dumps({"recovered":
+
+                           5.0, "sick": 4.0}, indent=1) == compute_answer(
         population=1000, confirmed_cases=100, recovered_people=50, dead_people=10
     )
